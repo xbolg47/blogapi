8e0984613438422246fae1df2783091668073c2b
xbolg.47 <xbolg.47@gmail.com>
2025-09-11 23:59:35 +0100

    Update Site Header

--- django_project/urls.py
+++ django_project/urls.py
@@ -17,6 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+admin.site.site_header = "Blog Admin"
+admin.site.site_title = "Blog Admin Portal"
+admin.site.index_title = "Welcome to Blog Portal"
+
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
